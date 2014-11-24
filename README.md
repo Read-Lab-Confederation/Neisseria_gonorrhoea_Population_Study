ae44e5a2900ff0647b429300b0e3628a7262d691
Matthew Ezewudo <mezewud@emory.edu>
2014-11-24 12:57:30 -0500

    Added license comments in Scripts

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 Neisseria_gonorrhoea_Population_Study
 
-Data Fles, Figures and supplementary materials on the study on Neisseria gonorrhoea population structure and antibiotics resistance evolution.
+Scripts and  materials on the study on Neisseria gonorrhoea population structure and antibiotics resistance evolution.
+
+1) blast_read.py
+    Parses antibiotic resistance database for matches to genes identified in 
+    BLAST search of sample set gene sequence database
+    
+2) parse.pl
+   Searches BLAST output for varint positions that indicate resistance mutation, 
+   when searching against a reference sequence for underlying resistance mutations.
+   
+3) noncore_parseAZ.py, noncore_parseCf.py,noncore_parseTet.py
+   Searches through non-core gene clusters in sample set for presence of 
+   horizontally transferred genes for the respective phenotypes of Azithromycin,
+   Cefexime and Tetracycline resistance.
+   
+4) pheno.R
+   Performs fisher's test for significance of association of presence 
+   of transfered gene and antibiotic resistance phenotype
--- pheno.R
+++ pheno.R
@@ -1,3 +1,26 @@
+
+# Copyright (c) 2014 Matthew Ezewudo <mezewud@emory.edu>
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+
 tet_table <- read.table("~/desktop/noncore/Tet_Resis_table")
 for (i in (1:nrow(tet_table))){
   result <-fisher.test(matrix(c(tet_table[i,2],tet_table[i,4],tet_table[i,3],tet_table[i,5]),2,2),alternative = "greater")
